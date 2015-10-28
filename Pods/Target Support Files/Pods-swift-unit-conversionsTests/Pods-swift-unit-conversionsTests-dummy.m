8c40bf2fa0442a687cdc1f1f0b8a3ec12ae26378
markedwardmurray <markedwardmurray@gmail.com>
2015-10-28 10:50:33 -0400

    pods: init with Quick & Nimble

---
+++ Pods/Target Support Files/Pods-swift-unit-conversionsTests/Pods-swift-unit-conversionsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_swift_unit_conversionsTests : NSObject
+@end
+@implementation PodsDummy_Pods_swift_unit_conversionsTests
+@end
---
+++ Pods/Target Support Files/Pods-swift-unit-conversionsTests/Pods-swift-unit-conversionsTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_swift_unit_conversionsTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_swift_unit_conversionsTestsVersionString[];
+

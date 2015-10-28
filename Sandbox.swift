abc0b17e7a5e6b65d57a53080204e1196aba326b
markedwardmurray <markedwardmurray@gmail.com>
2015-10-28 11:53:28 -0400

    write solution & tests

--- Sandbox.swift
+++ Sandbox.swift
@@ -7,3 +7,30 @@ import Foundation
 * Write your functions here!
 
 */
+
+func fahrenheitFromCelcius(celcius: Double) -> Double {
+    return (celcius * 9/5) + 32
+}
+
+func celciusFromFahrenheit(fahrenheit: Double) -> Double {
+    return (fahrenheit - 32) * 5/9
+}
+
+
+func radiansFromDegrees(degrees: Double) -> Double {
+    return degrees / 180 * M_PI
+}
+
+func degreesFromRadians(radians: Double) -> Double {
+    return radians / M_PI * 180
+}
+
+
+func milesFromKilometers(kilometers: Double) -> Double {
+    return kilometers / 1.60934
+}
+
+func kilometersFromMiles(miles: Double) -> Double {
+    return miles * 1.60934
+}
+

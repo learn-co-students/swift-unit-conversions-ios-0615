ecf3dfc428651c65811219ad73111766c6642bf9
Tim Clem <tim.clem@gmail.com>
2015-11-30 11:20:10 -0500

    Remove solution

--- swift-unit-conversions/Sandbox.swift
+++ swift-unit-conversions/Sandbox.swift
@@ -8,29 +8,5 @@ import Foundation
 
 */
 
-func fahrenheitFromCelsius(celsius: Double) -> Double {
-    return (celsius * 9/5) + 32
-}
 
-func celsiusFromFahrenheit(fahrenheit: Double) -> Double {
-    return (fahrenheit - 32) * 5/9
-}
-
-
-func radiansFromDegrees(degrees: Double) -> Double {
-    return degrees / 180 * M_PI
-}
-
-func degreesFromRadians(radians: Double) -> Double {
-    return radians / M_PI * 180
-}
-
-
-func milesFromKilometers(kilometers: Double) -> Double {
-    return kilometers / 1.60934
-}
-
-func kilometersFromMiles(miles: Double) -> Double {
-    return miles * 1.60934
-}
 
